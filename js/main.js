b731b3a2b8732df3ee7dc6f96e7a0bf37d7211ce
y1aszz <yasmim.sordi@estudante.ifms.edu.br>
2023-09-01 22:46:42 -0300

    chorando de tanto comentar

--- js/main.js
+++ js/main.js
@@ -31,21 +31,21 @@ function newMove(e){ //criando a função newMove referente ao botão
     const index = e.target.getAttribute("data-i");//esta pegando o atributo declarado no html
     e.target.innerHTML = player; //passando para o innerHTML a informação do player
     e.target.removeEventListener("click", newMove);//removendo o evento click do botão, para que não possa clicar duas vezes no mesmo lugar
-    select[index] = player;
+    select[index] = player;//armazenando
 
     setTimeout(() => {
-        check();
+        check();//sera executada depois que o restante
     },  [100]);
 
-    player = player === "X" ? "O" : "X";
-    corretPlayer.innerHTML = `Jogador da vez: ${player}`;
+    player = player === "X" ? "O" : "X";//se tiver o X selecionado vai ser o X senão será O
+    corretPlayer.innerHTML = `Jogador da vez: ${player}`;//a cada movimento troca a mensagem 
 }
 
 function check() {
-    let playerLasMove = player === "X" ? "O" : "X";
+    let playerLasMove = player === "X" ? "O" : "X";//executa depois que o de cima e pega o ultimo player que jogou 
 
     const items = select
-    .map((item, i) => [item, i])
+    .map((item, i) => [item, i]) //mapeia os itens selecionados
     .filter((item) => item[0] ===playerLasMove)
     .map((item) => item[1]);
 
